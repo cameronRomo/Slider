f2183a99c94f0ad60b141ab1e814d883c1a89e1c
Cameron Romo <cromo1225@gmail.com>
2022-01-06 20:15:12 -0700

    Add fake data

---
+++ components/dataSlider.js
@@ -0,0 +1,17 @@
+export const dataSlider = [
+  {
+    id: "7286898c-6eb7-11ec-90d6-0242ac120003",
+    title: "Lorem ipsum",
+    subTitle: "Lorem",
+  },
+  {
+    id: "72868c20-6eb7-11ec-90d6-0242ac120003",
+    title: "Lorem ipsum",
+    subTitle: "Lorem",
+  },
+  {
+    id: "72868f4a-6eb7-11ec-90d6-0242ac120003",
+    title: "Lorem ipsum",
+    subTitle: "Lorem",
+  },
+];
